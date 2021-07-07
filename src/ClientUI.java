ae65ac738da9c492d6db90db0968aeb3562c6b3c
Alan <86943620+alanoconner@users.noreply.github.com>
2021-07-08 02:52:21 +0900

    Start files

--- src/ClientUI.java
+++ src/ClientUI.java
@@ -1,3 +1,23 @@
+import javax.swing.*;
+
 public class ClientUI {
 
+    JFrame jFrame;
+    JTextArea msg_log;
+    JTextField msg_enter;
+
+    public static void main(String[] args) {
+        new ClientUI();
+    }
+
+    private ClientUI(){
+        jFrame = new JFrame("Client UI");
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setSize(500, 500);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setAlwaysOnTop(true);
+        jFrame.setVisible(true);
+
+
+    }
 }
